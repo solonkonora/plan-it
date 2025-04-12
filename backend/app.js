26a943625719809568b3d90d6450dbb3c7e1e9ec
solonkonora <nkwadanora@gmail.com>
2025-04-12 15:04:23 +0100

    initial commit:initialize the backend and add an app.js, install the following packages bcrypt cors dotenv express jsonwebtoken mongodb mongoose nodemon --save-dev, which are node.js backend with authentication, MongoDB integration, and development convenience features

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+// Create an instance of an Express app
+const app = express();
+
+const PORT = 3000;
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
