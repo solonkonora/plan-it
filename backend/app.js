d731e8280b0235d94fc1a501c50bf66e13f49f9b
solonkonora <nkwadanora@gmail.com>
2025-04-13 12:48:36 +0100

    created api endpoints, and tested with thunder bird to ensure all endpoints work as they should

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,24 @@
 import express from "express";
-import "./database/connection/mongo.js"
+import "./database/connection/mongo.js"; 
+import todoRoutes from "./routes/todo-routes.js";
 
 // Create an instance of an Express app
 const app = express();
 
 const PORT = 3000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Mount the todo routes under /api
+app.use('/api', todoRoutes);
+
 // Define a route for the root URL
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
--- backend/database/schemas/todo.js
+++ backend/database/schemas/todo.js
@@ -1,38 +1,9 @@
 import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    max_length: 255,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  created_at: {
-    type: Date,
-    default: Date.now
-  },
-  updated_at: {
-    type: Date,
-    default: Date.now
-  }
-}, {
-  timestamps: false
-});
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  // done: { type: Boolean, default: false },
+}, { timestamps: true });
 
-todoSchema.pre('save', function (next) {
-  this.updated_at = Date.now();
-  next();
-});
-
-// Define a pre-updateOne hook to update updated_at field
-todoSchema.pre('updateOne', function (next) {
-  this.update({}, { $set: { updated_at: Date.now() } });
-  next();
-});
-
-const Todo = mongoose.model('Todo', todoSchema);
-
-module.exports = Todo;
+export default mongoose.model('Todo', todoSchema);
