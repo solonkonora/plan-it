094a95e8609c89fcfce1cce49c04828cf0f8fa68
solonkonora <nkwadanora@gmail.com>
2025-04-14 04:48:41 +0100

    add the a todo component

--- angular-todo/src/app/app.component.ts
+++ angular-todo/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TodosComponent } from "./todos/todos.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
+  imports: [TodosComponent],
 })
 export class AppComponent {}
